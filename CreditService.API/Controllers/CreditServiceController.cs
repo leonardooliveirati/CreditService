ce2ff01b0b96355c75634d6acae655711fe298bd
leonardooliveirati <78557233+leonardooliveirati@users.noreply.github.com>
2024-02-20 00:03:42 -0300

    Serviço de Gestão de Crédito
    Funcionalidade de liberação de crédito:

---
+++ CreditService.API/Controllers/CreditServiceController.cs
@@ -0,0 +1,35 @@
+﻿using CreditService.Domain.Dto;
+using CreditService.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditServiceController : ControllerBase
+    {
+        private readonly IProcessamentoCreditoService _processamentoCréditoService;
+
+        public CreditServiceController(IProcessamentoCreditoService processamentoCreditoService)
+        {
+            _processamentoCréditoService = processamentoCreditoService;
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> PostAsync([FromBody] CreditoDto credit)
+        {
+            try
+            {
+                if (credit == null)
+                    return NotFound();
+
+                var resultado = await _processamentoCréditoService.ProcessarCredito(credit);
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CreditService.Domain/Interfaces/IProcessamentoCreditoService.cs
@@ -0,0 +1,14 @@
+﻿using CreditService.Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditService.Domain.Interfaces
+{
+    public interface IProcessamentoCreditoService
+    {
+        Task<ResultadoProcessamentoDto> ProcessarCredito(CreditoDto credito);
+    }
+}
